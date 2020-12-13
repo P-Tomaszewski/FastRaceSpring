e4c6289bd66f72881447ff32a1c5c7570e98c0d0
Patryk <ptomaszewskig3@gmail.com>
2020-12-13 02:06:34 +0100

    Add BS done

---
+++ src/main/java/p/tomaszewski/FastRace/controller/ProjectController.java
@@ -0,0 +1,14 @@
+package p.tomaszewski.FastRace.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+    @GetMapping
+    String showProjects(){
+        return "home";
+    }
+}
